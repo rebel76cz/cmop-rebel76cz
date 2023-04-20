76e9c21209853f3c21f31111692d43b6a0740df5
Martinn <martinhruska770@gmail.comgit config --global user.email martinhruska770@gmail.com>
2023-04-20 19:54:57 +0200

    u

--- app/Presenters/EditPresenter.php
+++ app/Presenters/EditPresenter.php
@@ -1,73 +1,107 @@
 <?php
+
 namespace App\Presenters;
 
 use Nette;
 use Nette\Application\UI\Form;
+use Nette\Database\Explorer;
+use Nette\Database\Table\ActiveRow;
 
 final class EditPresenter extends Nette\Application\UI\Presenter
 {
-	private Nette\Database\Explorer $database;
+    private Explorer $database;
 
-	public function __construct(Nette\Database\Explorer $database)
-	{
-		$this->database = $database;
-	}
+    public function __construct(Explorer $database)
+    {
+        parent::__construct();
+        $this->database = $database;
+    }
 
     protected function createComponentPostForm(): Form
-{
-	$form = new Form;
-	$form->addText('title', 'Titulek:')
-		->setRequired();
-	$form->addTextArea('content', 'Obsah:')
-		->setRequired();
+    {
+        $form = new Form;
+        $form->setMethod('POST');
+        $form->addUpload('file', 'Upload file')
+            ->addRule(Form::IMAGE, 'Only image files are allowed');
+        $form->addText('title', 'Title');
+        $form->addSubmit('submit', 'Submit');
+        $form->onSuccess[] = [$this, 'postFormSucceeded'];
 
-	$form->addSubmit('send', 'Uložit a publikovat');
-	$form->onSuccess[] = [$this, 'postFormSucceeded'];
+        $id = $this->getParameter('id');
+        if ($id) {
+            $post = $this->database->table('posts')->get($id);
+            if ($post) {
+                $form->setDefaults([
+                    'file' => '',
+                    'title' => $post->title,
+                ]);
+            }
+        }
 
-	return $form;
-}
+        return $form;
+    }
 
-public function postFormSucceeded(array $data): void
-{
-	$postId = $this->getParameter('postId');
+    public function postFormSucceeded(Form $form, array $data): void
+    {
+        $file = $data['file'];
+        $title = $data['title'];
+        $fileName = '';
 
-	if ($postId) {
-		$post = $this->database
-			->table('posts')
-			->get($postId);
-		$post->update($data);
+        if ($file->isOk() && !$file->isImage()) {
+            $form->addError('The uploaded file is not a valid image');
+            return;
+        }
 
-	} else {
-		$post = $this->database
-			->table('posts')
-			->insert($data);
-	}
+        if ($file->isOk()) {
+            $fileName = uniqid() . '.' . $file->getImageFileExtension();
+            $file->move(__DIR__ . '/../img/upload/' . $fileName);
+        }
 
-	$this->flashMessage('Příspěvek byl úspěšně publikován.', 'success');
-	$this->redirect('Post:show', $post->id);
-}
+        $id = $this->getParameter('id');
+        if ($id) {
+            $post = $this->database->table('posts')->get($id);
+            if ($post instanceof ActiveRow) {
+                $post->update([
+                    'file_name' => ($file->isOk()) ? $fileName : $post->file_name,
+                    'title' => $title,
+                ]);
+                $this->flashMessage('The item has been updated.', 'success');
+            } else {
+                $this->flashMessage('The item could not be found.', 'error');
+            }
+        } else {
+            if ($file->isOk()) {
+                $this->database->table('posts')->insert([
+                    'file_name' => $fileName,
+                    'title' => $title,
+                ]);
+                $this->flashMessage('The item has been created.', 'success');
+            } else {
+                $this->flashMessage('The uploaded file is not valid.', 'error');
+            }
+        }
 
-public function renderEdit(int $postId): void
-{
-	$post = $this->database
-		->table('posts')
-		->get($postId);
+        $this->redirect('this');
+    }
 
-	if (!$post) {
-		$this->error('Post not found');
-	}
+    public function renderEdit(int $id): void
+    {
+        $post = $this->database->table('posts')->get($id);
 
-	$this->getComponent('postForm')
-		->setDefaults($post->toArray());
-}
+        if (!$post instanceof ActiveRow) {
+            $this->error('Post not found');
+        }
 
-public function startup(): void
-{
-	parent::startup();
+        $this->getComponent('postForm')
+            ->setDefaults($post->toArray());
+    }
 
-	if (!$this->getUser()->isLoggedIn()) {
-		$this->redirect('Sign:in');
-	}
-}
+    protected function startup(): void
+    {
+        parent::startup();
 
+        if (!$this->getUser()->isLoggedIn()) {
+            $this->redirect('Sign:in');
+        }
+    }
 }
